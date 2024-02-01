60b7e463d14065726a3cf944f63796d4ec61395d
Tere Durán <teresitadejesus.dl8@gmail.com>
2024-02-01 14:02:49 -0600

    Otros archivos

---
+++ Prestamo.swift
@@ -0,0 +1,39 @@
+//
+//  Prestamo.swift
+//  Prestamos laboratorio
+//
+//  Created by Ios lab on 16/03/23.
+//
+
+import Foundation
+import CoreData
+import UIKit
+
+class PrestamoEquipos{
+    /*
+    public static let shared = PrestamoEquipos()
+    
+    //Conexión con la base de datos
+    func contexto() -> NSManagedObjectContext {
+        let delegate = UIApplication.shared.delegate as! AppDelegate
+        return delegate.persistentContainer.viewContext
+    }
+    
+    func guardarInformacionPrestamo(equipo: String, fecha: Date, idPrestamo: String, numeroEquipo: String) {
+        let context = contexto()
+        let entidadEquipo = NSEntityDescription.insertNewObject(forEntityName: "Prestamo", into: context) as! Prestamo
+        entidadEquipo.equipo = equipo
+        entidadEquipo.fecha = fecha
+        entidadEquipo.id_prestamo = idPrestamo
+        entidadEquipo.numero = numeroEquipo
+        
+        do{
+            try context.save()
+            print("Se guardó la información con éxito")
+        } catch let error as NSError {
+            print("No fue posible guardar la información", error.localizedDescription)
+        }
+        
+    }*/
+    
+}
